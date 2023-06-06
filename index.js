8abc322b72b323d5855bef42f3aeff7ad822f901
verezeeee <theusma58@gmail.com>
2023-06-06 15:12:31 -0300

    connect to database

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const userRoute = require("./src/routes/user.route")
-
+const connectDatabase = require('./src/database/db')
 const port = 3000
-
 const app = express()
+
+connectDatabase()
 app.use(express.json())
 app.use('/user', userRoute)
 
---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  console.log("Wait connecting to database");
+
+  mongoose.set('strictQuery', false)
+  mongoose
+    .connect(
+      "mongodb+srv://theusma58:<131344>@cluster0.lf86gdn.mongodb.net/?retryWrites=true&w=majority",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log("MongoDB Atlas  Connected"))
+    .catch((error) => console.log(error));
+};
+
+module.exports = connectDatabase;
